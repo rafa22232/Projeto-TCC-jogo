da0a214a52c6ff583ba97c72335952559d7a3d6a
rafa22232 <rafael.ferreira347@ete.sp.gov.br>
2025-10-20 18:55:20 -0300

    Subindo o jogo

---
+++ js/app.js
@@ -0,0 +1,82 @@
+
+function add() {
+	var n3 = document.getElementById('n3').value;
+	if (n3 != "") {
+		var n1 = Number(document.getElementById('n1').innerHTML);
+		var n2 = Number(document.getElementById('n2').innerHTML);
+		if((n1+n2) == n3){
+			alert("ACERTOU!");  
+		}else{
+			alert("ERROU!");
+		}
+		resetar();
+	}else{
+		alert("Preencha o campo")
+		resetar();
+	}
+}	
+function resetar() {
+	alt1 = Math.floor((Math.random() *35));
+	alt2 = Math.floor((Math.random() * 20 ));
+	document.getElementById("n3").value = "";
+	document.getElementById("n1").innerHTML = alt1;
+	document.getElementById("n2").innerHTML = alt2;
+}
+function resetardivisao() {
+	alt1 = Math.floor((Math.random() * 12))+1;
+	alt2 = Math.floor((Math.random() * 10 ))+1;
+	document.getElementById("n3").value = "";
+	document.getElementById("n1").innerHTML = alt1*alt2;
+	document.getElementById("n2").innerHTML = alt2;
+}
+
+function sub() {
+	var n3 = document.getElementById('n3').value;
+	if (n3 != "") {
+		var n1 = Number(document.getElementById('n1').innerHTML);
+		var n2 = Number(document.getElementById('n2').innerHTML);
+		if((n1-n2) == n3){
+			alert("ACERTOU!");
+		}else{
+			alert("ERROU!");
+		}
+		resetar();
+	}else{
+		alert("Preencha o campo")
+		resetar();
+	}
+}	
+
+function mult() {
+	var n3 = document.getElementById('n3').value;
+	if (n3 != "") {
+		var n1 = Number(document.getElementById('n1').innerHTML);
+		var n2 = Number(document.getElementById('n2').innerHTML);
+		if((n1*n2) == n3){
+			alert("ACERTOU!");
+		}else{
+			alert("ERROU!");
+		}
+		resetar();
+	}else{
+		alert("Preencha o campo")
+		resetar();
+	}
+}	
+
+function divisao() {
+	var n3 = document.getElementById('n3').value;
+	if (n3 != "") {
+		var n1 = Number(document.getElementById('n1').innerHTML);
+		var n2 = Number(document.getElementById('n2').innerHTML);
+		if((n1/n2) == n3){
+			alert("ACERTOU!");
+		}else{
+			alert("ERROU!");
+		}
+		resetardivisao();
+	}else{
+		alert("Preencha o campo")
+		resetardivisao();
+	}
+}	
